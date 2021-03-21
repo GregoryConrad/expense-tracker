bec830115803ad4215084f952366bb19a50fd6bf
GregoryConrad <gregorysconrad@gmail.com>
2021-03-21 19:28:45 -0400

    Added dummy test for CI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,12 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:expense_tracker/main.dart';
-
 void main() {
+  test('Dummy test', () => null);
 //  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 //    // Build our app and trigger a frame.
 //    await tester.pumpWidget(MyApp());
